5fb73d4d45ecb9edb7fb9e1b13d6f776d56930cd
Vasilii Feofanov <vasya3000-95@mail.ru>
2022-02-14 16:42:41 +0100

    added new self-learning "sklearn-like" module with new functionalities

---
+++ new-module/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""Self-learning module."""
+
+__author__ = """Vasilii Feofanov"""
+__email__ = 'vasilii.feofanov@gmail.com'
+__version__ = '0.1.0'
+
+from .self_learning import SelfLearning
+
+__all__ = ["SelfLearning"]
